f484ec51d972beb59fa8f5e5ec4bda72af65b9e9
gab759 <gabriel.cordova.e@tecsup.edu.pe>
2025-04-21 17:29:09 -0500

    add a construct game

--- src/pages/Game4.tsx
+++ src/pages/Game4.tsx
@@ -1,29 +1,30 @@
-import {Unity, useUnityContext} from "react-unity-webgl";
+import React from 'react';
 
-function Game4() {
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "/Builds.loader.js",
-        dataUrl: "/Builds.data",
-        frameworkUrl: "/Builds.framework.js",
-        codeUrl: "/Builds.wasm",
-    });
-
-
-
+const Game4: React.FC = () => {
     return (
-        <>
-            <div className="centered-container">
-                <div className="centered-content">
-                    <h1 className="centered-title">Game4</h1>
-                    <Unity unityProvider={unityProvider} className="centered-unity" />
-
-
-                </div>
-            </div>
-
-        </>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh' // Ocupa toda la altura de la ventana
+      }}>
+        <div style={{ position: 'relative', width: '50%', paddingBottom: '50%' }}>
+          <iframe
+            src="/Construct/index.html"
+            title="Construct 2 Game"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              border: 'none',
+            }}
+            allowFullScreen
+          />
+        </div>
+      </div>
     );
-}
-
-
-export default Game4
+  };
+  
+  export default Game4;
